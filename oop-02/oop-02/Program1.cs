3d77de2ea20e5d36fc72ad3258b9ac66648c93ae
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-09-16 12:11:33 +0300

    added class variables

--- oop-02/oop-02/Program1.cs
+++ oop-02/oop-02/Program1.cs
@@ -1,1 +1,12 @@
-﻿
+﻿partial class Date
+{
+    private int _year;
+    private int _month;
+    private int _day;
+    readonly int _hash = 0;
+    const string _format = "dd/mm/yyyy";
+    private string[] _months = {"января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября",
+        "октября", "ноября", "декабря"};
+    private int[] _daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static int _numberOfObjects = 0;
+}
