1bb76a902d3ff6712c45a3ce003168af4aba7c27
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-11 11:11:37 +0300

    project initialization

---
+++ oop-05/oop-05/basic classes.cs
@@ -0,0 +1,21 @@
+﻿interface ISwim
+{
+    void Honk(int count);
+    void CountIncome(double distance);
+}
+
+abstract class Vehicle : ISwim
+{
+    public int _Draught;
+    public string _Name = "";
+    public int _CaptainAge = 0;
+    public int _PassengersCapacity = 0;
+    public abstract void Honk(int count);
+    public abstract void CountIncome(double distance);
+}
+
+class Ship : Vehicle
+{
+    public override void Honk(int count) { }
+    public override void CountIncome(double distance) { }
+}
