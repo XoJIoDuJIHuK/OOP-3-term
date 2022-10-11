d03a4ce7ba6e95dfe51fd94b2432f6c8590b7997
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-11 11:12:05 +0300

    added class Captain

---
+++ oop-05/oop-05/Captain.cs
@@ -0,0 +1,25 @@
+﻿class Captain : ISwim
+{
+    public virtual void Smoke()
+    {
+        Console.WriteLine("Captain smokes");
+    }
+    public void Honk(int count)
+    {
+        Console.WriteLine("Captain screams 'Help!'");
+    }
+    public void CountIncome(double distance)
+    {
+        Console.WriteLine("Captain doesn't make income");
+    }
+    public int _Velocity { get; set; }
+    public Captain()
+    {
+        _Velocity = 1;
+    }
+    public override string ToString()
+    {
+        string str = "Captain Velocity = " + _Velocity.ToString();
+        return str;
+    }
+}
