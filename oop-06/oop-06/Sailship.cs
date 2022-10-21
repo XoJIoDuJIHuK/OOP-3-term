b56cdf938d2a3694c2f8bebdfb3170db2d2f144e
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-21 13:57:13 +0300

    project initialization

---
+++ oop-06/oop-06/Sailship.cs
@@ -0,0 +1,19 @@
+﻿class Sailship : Ship
+{
+    public new int _Draught = 200;
+    public Sailship(int draught, string name, int captainAge, int passengersCapacity)
+    {
+        if (captainAge < 0) throw new InvalidDataException("возраст капитана", "парсуник");
+        _PassengersCapacity = passengersCapacity;
+        _Draught = draught;
+        _Name = name;
+        _CaptainAge = captainAge;
+    }
+    public override string ToString()
+    {
+        string str = "Type: Sailship, name: " + _Name + ", passengers capacity: "
+            + _PassengersCapacity.ToString() + ", draught: " + _Draught.ToString() +
+            ", captain age: " + _CaptainAge.ToString();
+        return str;
+    }
+}
