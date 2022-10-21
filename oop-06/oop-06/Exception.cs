a681166b3267c450596c3499cb8cde15ff8dccfa
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-21 13:58:07 +0300

    added class Exception

---
+++ oop-06/oop-06/Exception.cs
@@ -0,0 +1,26 @@
+﻿class NullException : Exception
+{
+    public string _method = "";
+    public NullException(string method)
+    {
+        _method = method;
+    }
+}
+class InvalidDataException : Exception
+{
+    public string _shipType = "";
+    public string _field = "";
+    public InvalidDataException(string field, string shipType)
+    {
+        _field = field;
+        _shipType = shipType;
+    }
+}
+class ZeroCountException : Exception
+{
+    public string _method = "";
+    public ZeroCountException(string method)
+    {
+        _method = method;
+    }
+}
