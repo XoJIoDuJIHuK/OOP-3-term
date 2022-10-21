632dbde0cb5293ebdd18af575b9516e8aa5dba3e
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-21 13:58:19 +0300

    added class Steamvessel

---
+++ oop-06/oop-06/Steamvessel.cs
@@ -0,0 +1,19 @@
+﻿class Steamvessel : Ship
+{
+    public new int _Draught = 450;
+    public Steamvessel(int draught, string name, int captainAge, int passengersCapacity)
+    {
+        if (captainAge < 0) throw new InvalidDataException("возраст капитана", "корвет");
+        _PassengersCapacity = passengersCapacity;
+        _Draught = draught;
+        _Name = name;
+        _CaptainAge = captainAge;
+    }
+    public override string ToString()
+    {
+        string str = "Type: Steamvessel, name: " + _Name + ", passengers capacity: "
+            + _PassengersCapacity.ToString() + ", draught: " + _Draught.ToString() + 
+            ", captain age: " + _CaptainAge.ToString();
+        return str;
+    }
+}
