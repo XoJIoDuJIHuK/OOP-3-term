94f7fa572a6949945df6b16547baf08259186643
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-21 13:57:21 +0300

    added basic classes

---
+++ oop-06/oop-06/basic classes.cs
@@ -0,0 +1,16 @@
+﻿interface ISwim
+{
+}
+
+abstract class Vehicle : ISwim
+{
+    public int _Draught;
+    public string _Name = "";
+    public int _CaptainAge = 0;
+    public int _PassengersCapacity = 0;
+}
+
+class Ship : Vehicle
+{
+    
+}
