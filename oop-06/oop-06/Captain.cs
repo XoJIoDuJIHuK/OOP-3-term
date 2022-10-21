a880b0ceeab83a89af3eb61e840d85442e333942
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-10-21 13:57:45 +0300

    added class Captain

---
+++ oop-06/oop-06/Captain.cs
@@ -0,0 +1,13 @@
+﻿class Captain : ISwim
+{
+    public int _Velocity { get; set; }
+    public Captain()
+    {
+        _Velocity = 1;
+    }
+    public override string ToString()
+    {
+        string str = "Captain Velocity = " + _Velocity.ToString();
+        return str;
+    }
+}
