44f65b9ae82b3e1a23383a137f1046f46e068633
XoJIoDuJIHuK <XoJIoDuJIHuK@yandex.by>
2022-11-18 16:11:24 +0300

    added task 1 to Program

--- oop-10/oop-10/Program.cs
+++ oop-10/oop-10/Program.cs
@@ -3,32 +3,33 @@
     static void Main()
     {
         // Task01
-        //string[] months = { "January", "February", "March", "April", "May", "June", "July",
-        //    "August", "September", "October", "November", "December" };
-        //IEnumerable<string> sumWinMonths = months.Where(n => Equals(n, "January") || Equals(n, "February")
-        //|| Equals(n, "December") || Equals(n, "June") || Equals(n, "July") || Equals(n, "August"))
-        //    .Select(n => n);
-        //Console.Write("N = ");
-        //int N = int.Parse(Console.ReadLine());
-        //IEnumerable<string> lengthEqualsN = months.Where(n => n.Length == N).Select(n => n);
-        //IEnumerable<string> lengthMoreOrEquals4AndContainsU = months.Where(n => n.Length >= 4 
-        //    || n.IndexOf('u') != -1).Select(n => n);
-        //IEnumerable<string> alphOrder = months.OrderBy(s => s);
-        //foreach (string month in sumWinMonths) Console.WriteLine(month);
-        //Console.WriteLine();
-        //foreach (string month in lengthEqualsN) Console.WriteLine(month);
-        //Console.WriteLine();
-        //foreach (string month in alphOrder) Console.WriteLine(month);
+        string[] months = { "January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December" };
+        IEnumerable<string> sumWinMonths = months.Where(n => Equals(n, "January") || Equals(n, "February")
+        || Equals(n, "December") || Equals(n, "June") || Equals(n, "July") || Equals(n, "August"))
+            .Select(n => n);
+        Console.Write("N = ");
+        int N = int.Parse(Console.ReadLine());
+        IEnumerable<string> lengthEqualsN = months.Where(n => n.Length == N).Select(n => n);
+        IEnumerable<string> lengthMoreOrEquals4AndContainsU = months.Where(n => n.Length >= 4 
+            || n.IndexOf('u') != -1).Select(n => n);
+        IEnumerable<string> alphOrder = months.OrderBy(s => s);
+        foreach (string month in sumWinMonths) Console.WriteLine(month);
+        Console.WriteLine();
+        foreach (string month in lengthEqualsN) Console.WriteLine(month);
+        Console.WriteLine();
+        foreach (string month in alphOrder) Console.WriteLine(month);
 
         //Task02
-        var rand = new Random();
-        List<Date> dateList = new();
-        for (int i = 0; i < 10; i++) dateList.Add(new Date(rand.Next(1945, 2076), rand.Next(1, 13), 
-            rand.Next(1, 28)));
+        //var rand = new Random();
+        //List<Date> dateList = new();
+        //for (int i = 0; i < 10; i++) dateList.Add(new Date(rand.Next(1945, 2076), rand.Next(1, 13), 
+        //    rand.Next(1, 28)));
         foreach(var n in dateList) Console.WriteLine(n);
         Console.Write("year = ");
         int year = int.Parse(Console.ReadLine());
-        IEnumerable<Date> certainYearDates = dateList.Where(n => n.getYear() == year).Select(n => n);
+        //IEnumerable<Date> certainYearDates = dateList.Where(n => n.getYear() == year).Select(n => n);
+        IEnumerable<Date> certainYearDates = from d in dateList where d.getYear() == year select d;
         foreach (Date n in certainYearDates) n.WriteDate();
         Console.Write("month = ");
         int iMonth = int.Parse(Console.ReadLine());
